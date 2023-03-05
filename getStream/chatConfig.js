b8efd863d289f69d16d4ccf1e6bd38a23a931ce0
roihan37 <pro.roihan@gmail.com>
2023-03-05 19:12:22 +0700

    tes chat getstream

---
+++ getStream/chatConfig.js
@@ -0,0 +1,7 @@
+// chatConfig.js
+export const chatApiKey = '23p7uvpe9az3';
+export const chatUserId = 'demoTestUserOne';
+export const chatUserToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZGVtb1Rlc3RVc2VyT25lIn0.7Y_iwKss4yExvd79I8MsiodVxFqC0lkQ5d3wVhlnt1w";
+export const chatUserName = 'demoTestUserOne';
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZGVtb1VzZXJPbmUifQ.kDbBeFai6JRUz-fYU_CT4sxQKOKiIu1zL0yp0TEylaY
